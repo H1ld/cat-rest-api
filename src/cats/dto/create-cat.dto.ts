c1f8fe57254e2b5522877eab741f047dd64cdd54
H1ld <HildrichSalmon@gmail.com>
2025-11-13 15:03:24 +0100

    fixed cats creation from last commit

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,4 +1,12 @@
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCatDto {
+  @ApiProperty({ example: 'Charlie', description: 'Name of the cat' })
+  @IsString()
   public name: string;
+
+  @ApiProperty({ example: 'white', description: 'Color of the cat' })
+  @IsString()
   public color: string;
 }
